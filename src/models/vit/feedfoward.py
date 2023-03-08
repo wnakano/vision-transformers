e6d04da9a5235d0b32f8ab8ebb158e077f8fc12e
wnakano <nknvolpi@gmail.com>
2023-03-08 01:46:27 -0300

    feat: chaging inner_dim to inner_dim_scale

--- src/models/vit/feedfoward.py
+++ src/models/vit/feedfoward.py
@@ -4,15 +4,15 @@ class FeedFoward(nn.Module):
     def __init__(
         self,
         dim: int, 
-        inner_dim: int,
+        inner_dim_scale: int,
         dropout: float = .0) -> None:
         super().__init__()
 
         self.linear_seq = nn.Sequential(
-            nn.Linear(dim, inner_dim),
+            nn.Linear(dim, inner_dim_scale),
             nn.GELU(),
             nn.Dropout(dropout),
-            nn.Linear(inner_dim, dim),
+            nn.Linear(inner_dim_scale, dim),
             nn.Dropout(dropout)
         )
     
